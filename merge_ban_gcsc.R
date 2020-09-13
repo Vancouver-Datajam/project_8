30be7cc0d9f0ce095ee5994c908f1708a5c26205
Keegan Korthauer <kdkorthauer@gmail.com>
2020-09-12 17:31:58 -0700

    Create merge_ban_gcsc.R

---
+++ merge_ban_gcsc.R
@@ -0,0 +1,29 @@
+##########################################
+######Combine the data sets;
+#########################################
+​
+gcsc_clean <- read_csv(file = "cleaned_gcsc_data.csv")
+​
+ban_data <- read_csv(file = here("project_8-master/ban_data.csv")) %>% 
+  rename("ban_type" = Type,
+         "effective_date" = `Effective Date`)
+​
+ban_wide <- ban_data %>% 
+  pivot_wider(names_from = ban_type, 
+              values_from = effective_date) %>% 
+  mutate_at(3:5, funs(dmy)) %>% 
+  mutate(ban_date = coalesce(`Plastics straws`, `Plastic bags`, `Food packaging`))
+​
+gcsc_ban_com <- gcsc_clean %>% 
+  rename("City" = nearest_city, "Province" = province) %>% 
+  left_join(ban_wide, by =c("City", "Province")) %>%
+  select(Province, City, cleanup_date, date, ban_date, `Plastics straws`, `Plastic bags`, `Food packaging`, everything()) %>% 
+  mutate(ban_in_place = if_else((ban_date <= date|is.na(ban_date)==T), "N", "Y"), 
+         straw_ban = ifelse((`Plastics straws` <= date|is.na(`Plastics straws`)==T), "N", "Y"), 
+         bags_ban = ifelse((`Plastic bags` <= date|is.na(`Plastic bags`)==T), "N", "Y"), 
+         food_pack_ban = ifelse((`Food packaging` <= date|is.na(`Food packaging`)==T), "N", "Y")
+         ) %>%
+  select(Province, City, cleanup_date, date, ban_date, ban_in_place, straw_ban, bags_ban, food_pack_ban, everything()) %>% 
+  select(-c(`Plastics straws`, `Plastic bags`, `Food packaging`)) %>% 
+  rename(cleanup_date_tm = cleanup_date, 
+         cleanup_date = date)
